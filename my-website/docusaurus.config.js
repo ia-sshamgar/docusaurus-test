646d0dd02b55840ced49412409ab9b09f0f5a5d6
ia-sshamgar <sshamgar@inductiveautomation.com>
2022-10-26 13:56:27 -0500

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ia-sshamgar', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-test', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -41,7 +43,7 @@ const config = {
             'https://github.com/ia-sshamgar/docusaurus-test',
           sidebar: {
             hideable: true,
-            
+
 
           }
         },
